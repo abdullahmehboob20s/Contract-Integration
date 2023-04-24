51b04a9c1471d061f7de569c6f50e6cd18efcc14
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-04-24 20:14:42 +0500

    added switching network

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import { useAccount } from "wagmi";
+import {
+  useAccount,
+  useChainId,
+  useConnect,
+  useNetwork,
+  useSwitchNetwork,
+} from "wagmi";
 import ConnectButton from "./components/ConnectButton";
 import AfterConnectContent from "components/AfterConnectContent";
+import { bscTestnet } from "wagmi/chains";
 
 function App() {
   const { address } = useAccount();
+  const { chain } = useNetwork();
+  const { switchNetwork, isLoading: isSwitching } = useSwitchNetwork();
+
+  if (chain.id !== bscTestnet.id) {
+    return (
+      <div>
+        <h1>BSC Testnet Supported</h1>
+        <button
+          onClick={() => switchNetwork(bscTestnet.id)}
+          disabled={isSwitching}
+        >
+          Switch to BNB Testnet
+        </button>
+      </div>
+    );
+  }
 
   return <div>{!address ? <ConnectButton /> : <AfterConnectContent />}</div>;
 }
